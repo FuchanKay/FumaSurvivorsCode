46b95b71f2a0b45b25ef77274dbaa53974721551
FuchanKay <kazeuma0923@gmail.com>
2025-02-10 19:50:59 -0500

    Delete SoftwareOne/OsuCseWsTemplate/workspace/PhysicsEngine directory

--- SoftwareOne/OsuCseWsTemplate/workspace/PhysicsEngine/src/com/fuchankay/game/Utilities.java
+++
@@ -1,9 +0,0 @@
-package com.fuchankay.game;
-
-public class Utilities {
-    private Utilities() {}
-    public static final float magnitude(float x, float y) {
-        return (float)(Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)));
-    }
-    
-}
